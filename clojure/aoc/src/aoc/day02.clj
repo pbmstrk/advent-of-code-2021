a96b68bb4810766ac0829ab39342bad935ceeaf7
pbaumstark <41694868+pbmstrk@users.noreply.github.com>
2022-11-04 00:54:30 -0700

    add clojure day2

---
+++ clojure/aoc/src/aoc/day02.clj
@@ -0,0 +1,41 @@
+(ns aoc.day02
+  (:require [clojure.java.io]
+            [clojure.string :as str]))
+
+(defrecord Position [horizontal depth])
+(defrecord PositionWithAim [horizontal depth aim])
+
+(defn vec->command [[dir mag]]
+  {:direction dir :magnitude (Integer/parseInt mag)})
+
+(defn parse [input]
+  (->> input
+       str/split-lines
+       (map #(clojure.string/split % #" "))
+       (map vec->command)))
+
+(defn update-position [position instruction]
+  (let [{horizontal :horizontal depth :depth} position,
+        {direction :direction magnitude :magnitude} instruction]
+    (case direction
+      "forward" (->Position (+ horizontal magnitude) depth)
+      "down" (->Position horizontal (+ depth magnitude))
+      "up" (->Position horizontal (- depth magnitude)))))
+
+(defn update-position-with-aim [position instruction]
+  (let [{horizontal :horizontal depth :depth aim :aim} position,
+        {direction :direction magnitude :magnitude} instruction]
+    (case direction
+      "forward" (->PositionWithAim (+ horizontal magnitude) (+ depth (* aim magnitude)) aim)
+      "down" (->PositionWithAim horizontal depth (+ aim magnitude))
+      "up" (->PositionWithAim horizontal depth (- aim magnitude)))))
+
+(defn get-result [final-state]
+  (let [{horizontal :horizontal depth :depth} final-state]
+    (* horizontal depth)))
+
+(defn -main []
+  (let [commands (parse (slurp "resources/day02.txt"))
+        final-position (reduce update-position (->Position 0 0) commands)
+        final-position-with-aim (reduce update-position-with-aim (->PositionWithAim 0 0 0) commands)]
+    (run! println (map get-result [final-position final-position-with-aim]))))
