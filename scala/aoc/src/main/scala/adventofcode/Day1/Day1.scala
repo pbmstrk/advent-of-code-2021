1143553ec230b1c34b62d02ebe251e9bdfdbbb30
pbaumstark <41694868+pbmstrk@users.noreply.github.com>
2022-11-04 00:38:43 -0700

    add scala day1

---
+++ scala/aoc/src/main/scala/adventofcode/Day1/Day1.scala
@@ -0,0 +1,17 @@
+package adventofcode.Day1
+
+import scala.io.Source
+import scala.util.Using
+
+def countIncreases(lst: List[Int], n: Int): Int =
+  lst.zip(lst.drop(n)).filter((a, b) => b > a).length
+
+@main def day01(): Unit =
+  val input = Using.resource(Source.fromFile("input/day01.txt"))(_.getLines.toList)
+  val measurements = input.map(_.toInt)
+
+  val part1 = countIncreases(measurements, 1)
+  val part2 = countIncreases(measurements, 3)
+  println(part1)
+  println(part2)
+
